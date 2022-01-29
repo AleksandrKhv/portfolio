95f7919f34add020b4ff24a90a419d1cbd4d9425
AleksandrKhv <sidby83@gmail.com>
2022-01-29 12:46:29 +0100

    step1.5.1

--- src/components/сontacts/Contacts.js
+++ src/components/сontacts/Contacts.js
@@ -8,9 +8,9 @@ const Contacts = () => {
             <div className={st.contacts + ' ' + stContainer.container}>
                 <h3>Contacts</h3>
                     <form className={st.message} >
-                        <input type="text"/>
-                        <input type="text"/>
-                        <textarea></textarea>
+                        <input type="text" placeholder={'Your name'}/>
+                        <input type="text" placeholder={'Your e-mail'}/>
+                        <textarea placeholder={'write me'}></textarea>
                     </form>
                 <a href="#" className={st.btn} >
                     <span/>
